2d56aa9a0175f5435d31aeea0c79a4d98b28182d
h3athen <gurungaryan73@gmail.com>
2022-07-28 11:55:26 +0530

    logs all syscalls

---
+++ syslog-ebpf/src/main.rs
@@ -0,0 +1,37 @@
+#![no_std]
+#![no_main]
+
+use core::slice;
+
+use aya_bpf::{
+    macros::tracepoint,
+    programs::TracePointContext, BpfContext,
+};
+use aya_log_ebpf::info;
+
+#[tracepoint(name="syslog")]
+pub fn syslog(ctx: TracePointContext) -> u32 {
+    match unsafe { try_syslog(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_syslog(ctx: TracePointContext) -> Result<u32, u32> {
+    // Parsing the arguments
+    let args = slice::from_raw_parts(ctx.as_ptr() as *const usize, 2);
+
+    let syscall     = args[1] as u64;
+    let pid         = ctx.pid();
+    let message = ctx.command().map_err(|e| e as u32)?;
+    let message    = core::str::from_utf8_unchecked(&message[..]);
+
+    info!(&ctx, "syscall: {} | pid: {} | message: {}",syscall,pid,message);
+
+    Ok(0)
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
---
+++ syslog/src/main.rs
@@ -0,0 +1,50 @@
+use aya::{include_bytes_aligned, Bpf};
+use aya::programs::TracePoint;
+use aya_log::BpfLogger;
+use clap::Parser;
+use log::info;
+use simplelog::{ColorChoice, ConfigBuilder, LevelFilter, TermLogger, TerminalMode};
+use tokio::signal;
+
+#[derive(Debug, Parser)]
+struct Opt {
+    
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let opt = Opt::parse();
+
+    TermLogger::init(
+        LevelFilter::Debug,
+        ConfigBuilder::new()
+            .set_target_level(LevelFilter::Error)
+            .set_location_level(LevelFilter::Error)
+            .build(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )?;
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Bpf::load_file` instead.
+    #[cfg(debug_assertions)]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/debug/syslog"
+    ))?;
+    #[cfg(not(debug_assertions))]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/release/syslog"
+    ))?;
+    BpfLogger::init(&mut bpf)?;
+    let program: &mut TracePoint = bpf.program_mut("syslog").unwrap().try_into()?;
+    program.load()?;
+    program.attach("raw_syscalls", "sys_enter")?;
+
+    info!("Waiting for Ctrl-C...");
+    signal::ctrl_c().await?;
+    info!("Exiting...");
+
+    Ok(())
+}
